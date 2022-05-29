15288c6cefb6bb214fe5e50eff617c8de95da368
vishalhgajera <vishgajera@gmail.com>
2022-05-29 18:04:54 +0530

    Add all my files

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatButtonModule} from '@angular/material/button';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RoleComponent } from './role/role.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RoleComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatTableModule,
+    MatPaginatorModule,
+    BrowserAnimationsModule,
+    MatSortModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/role/role.component.ts
@@ -0,0 +1,136 @@
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { FormControl,FormGroup } from '@angular/forms';
+import {MAT_DATE_LOCALE} from '@angular/material/core';
+export interface Role {
+  id: string;
+  name: string;
+  creation_date: Date;
+  type: string;
+};
+const types: string[] = [
+  'Admin',
+  'User',
+];
+const names: string[] = [
+  'Maia',
+  'Asher',
+  'Olivia',
+  'Atticus',
+  'Amelia',
+  'Jack',
+  'Charlotte',
+  'Theodore',
+  'Isla',
+  'Oliver',
+  'Isabella',
+  'Jasper',
+  'Cora',
+  'Levi',
+  'Violet',
+  'Arthur',
+  'Mia',
+  'Thomas',
+  'Elizabeth',
+];
+
+@Component({
+  selector: 'app-role',
+  templateUrl: './role.component.html',
+  styleUrls: ['./role.component.scss'],
+  providers:[
+    { provide: MAT_DATE_LOCALE, useValue: 'en-GB' }
+  ]
+})
+
+export class RoleComponent implements OnInit, AfterViewInit {
+
+  displayedColumns: string[] = ['id', 'name', 'creation_date', 'type'];
+  dataSource: MatTableDataSource<Role>;
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+  @ViewChild(MatSort)
+  sort!: MatSort;
+  minDate: Date;
+  maxDate: Date;
+  range = new FormGroup({
+    start: new FormControl(),
+    end: new FormControl(),
+  });
+  searchData!: string;
+  // Create 100 users
+  users: Role[] = Array.from({ length: 100 }, (_, k) => this.createRandomData(k + 1));
+
+  constructor() {
+
+    this.dataSource = new MatTableDataSource(this.users);
+
+    this.maxDate = new Date();
+    this.minDate = this.minDateFinder();
+  
+  }
+
+  ngAfterViewInit() {
+    this.setFilter();
+  }
+
+  setFilter() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  dateFilter() {
+    this.searchData = "" ;
+    const dateRangeData = this.users.filter(x => {
+      const dateTime = x.creation_date.getTime();
+      const startDate = this.range.value.start.getTime();
+      const endDate = this.range.value.end.getTime();
+      return startDate < dateTime && dateTime < endDate ;
+    });
+    this.dataSource = new MatTableDataSource(dateRangeData);
+    this.setFilter();
+
+  }
+  
+  minDateFinder() {
+    const minValue = Math.min(...this.users.map(data => data.creation_date.getTime()));
+    return new Date(minValue) ;
+  }
+
+  ngOnInit(): void {
+  }
+
+  createRandomData(id: number): Role {
+    const randomName =
+      names[Math.round(Math.random() * (names.length - 1))] +
+      ' ' +
+      names[Math.round(Math.random() * (names.length - 1))].charAt(0) +
+      '.';
+
+    return {
+      id: id.toString(),
+      name: randomName,
+      creation_date: this.randomDate(),
+      type: types[Math.round(Math.random() * (types.length - 1))],
+    };
+  }
+  randomDate() {
+    const date = new Date().getDate() + 1;
+    const month = new Date().getMonth() + 1;
+    const year = new Date().getFullYear() - 1;
+    return new Date(Math.round((Math.random())) + year, Math.random() * month, Math.random() * date);
+  }
+
+}
